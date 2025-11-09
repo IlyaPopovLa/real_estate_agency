6c9d8d6a5e27ffe065d03a0e41b708f6ca597308
IlyaPopovLa <il.popov@lamoda.ru>
2025-11-09 09:14:56 +0300

    filtering in admin UI

--- property/admin.py
+++ property/admin.py
@@ -10,3 +10,4 @@ class FlatAdmin(admin.ModelAdmin):
     new_building = ['new_building']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
